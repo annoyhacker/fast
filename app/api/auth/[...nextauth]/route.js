d71e81a05858a159aba612d97ffc1c809f9e0ed5
annoyhacker <annoyhacker190@gmail.com>
2025-05-04 00:01:02 +0530

    remove auth

--- app/api/auth/[...nextauth]/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-import NextAuth from 'next-auth'
-import { authOptions } from '@/lib/authOptions'
-
-export { GET, POST }
-    from "next-auth/next"
-export const handler = NextAuth(authOptions)
